f62c65db100af865aac3401999a5e6c635fccb47
md sajid ahsan seyam <129988169+codeNina42@users.noreply.github.com>
2025-10-24 13:44:34 +0600

    Create class2_login.php

---
+++ class2_login.php
@@ -0,0 +1,25 @@
+<?php 
+echo "db saved information: <br>"; 
+$dbUserName = "seyam@gmail.com";
+$dbPassword = "123";
+
+echo $dbUserName;
+echo "<br>";  // Added line breaks for better readability
+echo $dbPassword;
+echo "<br>";
+echo "user input saved<br>";
+echo "user input information <br>";
+$user = $_REQUEST['username'];
+$pass = $_REQUEST['password'];
+
+echo $user;
+echo "<br>";  // Added line breaks for better readability
+echo $pass;
+echo "<br>";
+
+if ($dbUserName == $user && $dbPassword == $pass) {  // Fixed comparison operator
+    echo '<font color="green">your login is successful</font>';  // Fixed quotes
+} else {
+    echo '<font color="red">log in failed</font>';  // Fixed quotes
+}
+?>
